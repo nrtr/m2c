b3e151b8aee418f86b49614825cb3d79e52d60de
nrtr <107276800+nrtr@users.noreply.github.com>
2023-02-19 22:31:54 -0500

    Create script_explaint.sql

---
+++ script_explaint.sql
@@ -0,0 +1,16 @@
+EXPLAIN SELECT
+airline.airlinename,
+AVG(datediff(departure,birthdate)/365.25) as avg_age,
+count(*) as nb_people
+FROM
+booking, flight, airline, passengerdetails
+WHERE
+booking.flight_id=flight.flight_id AND
+airline.airline_id=flight.airline_id AND
+booking.passenger_id=passengerdetails.passenger_id AND
+country IN ("GERMANY", "SPAIN", "GREECE")
+GROUP BY
+airline.airlinename
+ORDER BY
+airline.airlinename, avg_age
+LIMIT 10;
